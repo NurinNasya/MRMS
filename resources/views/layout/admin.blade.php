fc6add9212ba7c4f62f37de31d2f4d46f1b6b8c6
NurinNasya <nurinnasya17@gmail.com>
2025-04-13 11:29:50 +0800

    lil

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MRMS ADMIN</title>
 
-    <!-- Vite CSS and JS -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Vite CSS and JS -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 <body class="font-sans bg-gray-100">
